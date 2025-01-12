f40300fd13d755de212814038d52fae020b83703
Titus Nguyen <titus.tnguyen17@gmail.com>
2025-01-12 16:56:19 -0700

    ElevatorIOSim fix + other fixes

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -1,5 +1,7 @@
 package frc.robot.constants;
 
+import edu.wpi.first.math.system.plant.DCMotor;
+
 public class ElevatorConstants {
     public static double kP;
     public static int kElevatorMotorPort;
@@ -7,4 +9,8 @@ public class ElevatorConstants {
     public static boolean kEncoderReversed;
     public static double kShooterToleranceRPS;
     public static double kEncoderDistancePerPulse;
+    public static DCMotor elevator1Gearbox;
+    public static double elevator1MotorReduction;
+    public static DCMotor elevator2Gearbox;
+    public static double elevator2MotorReduction;
 }
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem/ElevatorIOSim.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem/ElevatorIOSim.java
@@ -2,14 +2,20 @@ package frc.robot.subsystems.ElevatorSubsystem;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
-import frc.robot.subsystems.DriveSubsystem.ModuleIO;
+import frc.robot.constants.ElevatorConstants;
+
 
 public class ElevatorIOSim implements ElevatorIO {
     private static final double LOOP_PERIOD_SECS = 0.02;
 
-    private final DCMotorSim elevatorMotor1Sim = new DCMotorSim(DCMotor.getNEO(2), 6.75, 0.025);
-    private final DCMotorSim elevatorMotor2Sim = new DCMotorSim(DCMotor.getNEO(2), 6.75, 0.025);
+    private final DCMotorSim elevatorMotor1Sim = new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(ElevatorConstants.elevator1Gearbox, 0.025, ElevatorConstants.elevator1MotorReduction),
+            ElevatorConstants.elevator1Gearbox);
+    private final DCMotorSim elevatorMotor2Sim = new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(ElevatorConstants.elevator2Gearbox, 0.025, ElevatorConstants.elevator2MotorReduction),
+            ElevatorConstants.elevator2Gearbox);
     private double elevatorAppliedVolts = 0.0;
 
     @Override
