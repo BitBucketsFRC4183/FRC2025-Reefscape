e3b335c87fd6a4f8a07c55e05c6a0c547956162e
Triple-AAidan <aidantaehwalee@gmail.com>
2025-01-18 16:20:34 -0700

    George finish commands

--- src/main/java/frc/robot/commands/ElevatorSetPointCommand.java
+++ src/main/java/frc/robot/commands/ElevatorSetPointCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import com.revrobotics.spark.SparkBase;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.constants.ElevatorConstants;
@@ -10,31 +11,27 @@ import frc.robot.subsystems.ElevatorSubsystem.ElevatorIOSparkMax;
 import frc.robot.subsystems.ElevatorSubsystem.ElevatorEncoderIO;
 
 public class ElevatorSetPointCommand  extends Command {
-    private final double setpoint;
+    private final double setpoint = 0;
     private final ElevatorIOSparkMax elevatorIOSparkMax = new ElevatorIOSparkMax(ElevatorConstants.elevatorSpark1,ElevatorConstants.elevatorSpark2,  ElevatorIOSparkMax.timestampQueue,ElevatorIOSparkMax.elevatorPositionQueue) ;
     public ElevatorSubsystem elevator = new ElevatorSubsystem(elevatorIOSparkMax);
     // private final ElevatorIOSparkMax sparkMaxencoder;
     // Constructor with parameters
-    public ElevatorSetPointCommand(ElevatorSubsystem elevator, double setpoint) {
-        this.setpoint = setpoint;
+    public ElevatorSetPointCommand(ElevatorSubsystem elevator, int setpointnumber) {
         this.elevator = elevator;
         addRequirements(elevator);
-        if (elevator.elevatorXbox.getAButton()) {
-            elevator.profileGoal = new TrapezoidProfile.State(ElevatorConstants.L1, 0);}
-            setpoint = elevator.elevatorProfile.calculate(ElevatorConstants.kDt, ElevatorConstants.L1, elevator.profileGoal);
-        else if (elevator.elevatorXbox.getXButton()){
-            elevator.profileGoal = new TrapezoidProfile.State(ElevatorConstants.L3, 0);}
-        else if (elevator.elevatorXbox.getYButton()){
-            elevator.profileGoal = new TrapezoidProfile.State(ElevatorConstants.L4, 0);}
-        }
-
-    public void intialize(){
-        elevator.elevatorFeedback.setSetpoint(setpoint);
-    }
+        if (setpointnumber == 1) {
+            elevator.profileGoal = new TrapezoidProfile.State(ElevatorConstants.L1, 0);
+            elevator.profileSetPoint = elevator.elevatorProfile.calculate(ElevatorConstants.kDt, elevator.profileSetPoint, elevator.profileGoal);}
+        else if (setpointnumber == 2){
+            elevator.profileGoal = new TrapezoidProfile.State(ElevatorConstants.L3, 0);
+            elevator.profileSetPoint = elevator.elevatorProfile.calculate(ElevatorConstants.kDt, elevator.profileSetPoint, elevator.profileGoal);}
+        else if (setpointnumber == 3){
+            elevator.profileGoal = new TrapezoidProfile.State(ElevatorConstants.L4, 0);
+            elevator.profileSetPoint = elevator.elevatorProfile.calculate(ElevatorConstants.kDt, elevator.profileSetPoint, elevator.profileGoal);}}
 
     public void execute(){
-        // double power = elevator.elevatorFeedback.calculateOutput(elevator.);
-        // elevator.moveElevator(power);
+        elevator.MoveElevatorToSetpoint(int profileSetPoint) {
+        }
     }
 
     public boolean isFinished() {
@@ -44,3 +41,4 @@ public class ElevatorSetPointCommand  extends Command {
 
 
 
+t
