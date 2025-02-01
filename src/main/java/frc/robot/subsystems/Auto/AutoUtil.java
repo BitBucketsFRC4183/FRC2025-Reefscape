b5fb0c126d174686d02beae15f09ae7cb3fce2b0
therese <therese.bunyi@icloud.com>
2025-01-31 17:53:34 -0700

    autochooser works

--- src/main/java/frc/robot/subsystems/Auto/AutoUtil.java
+++ src/main/java/frc/robot/subsystems/Auto/AutoUtil.java
@@ -5,6 +5,7 @@ import choreo.auto.AutoFactory;
 import choreo.trajectory.SwerveSample;
 import choreo.trajectory.TrajectorySample;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.Subsystem;
@@ -15,40 +16,10 @@ import java.util.function.Supplier;
 
 
 public class AutoUtil {
-    private static AutoFactory AutoFactory;
-    private static AutoChooser AutoChooser;
-
-    public static void initAuto() {
-        setupFactory();
-        setupChooser();
-    }
-
-    /*public static AutoChooser getChooser() {
-
-        return
-    }
-
-    public static AutoFactory getAutoFactory() {
-
-        return AutoFactory;
-    }*/
-
-    private static void setupFactory() {
-        AutoFactory.bind("Marker", Commands.print(
-                "Marker Passed"));
-    }
-
-    private static void setupChooser() {
-        // interface for choreo
-
-        // Made sendable, use SmartDashboard now
-//        chooser.addCmd("My Routine", () -> Autos.getMyRoutine());
-//        chooser.addCmd("Print", () -> Autos.getPrint());
-//        chooser.addCmd("Split", () -> Autos.getSplitRoutine());
-//        chooser.addCmd("Straight", () -> Autos.getStraight());
-//        // Default
-//        chooser.select("Straight");
-
+    AutoChooser autoChooser = new AutoChooser();
+    /*private final AutoFactory autoFactory;
+    private final AutoChooser autoChooser;
+    public initAuto() {
         AutoChooser autoChooser = new AutoChooser();
         AutoChooser.addRoutine("FourL4CoralBottomRoutine", AutoSubsystem::FourL4CoralBottomRoutine);
         AutoChooser.addRoutine("FourL4CoralTopRoutine", AutoSubsystem::FourL4CoralBottomRoutine);
@@ -59,10 +30,30 @@ public class AutoUtil {
                 AutoSubsystem::deposit);
         AutoChooser.addCmd("claw",
                 AutoSubsystem::claw);
-        SmartDashboard.putData(AutoChooser);
+        SmartDashboard.putData(AutoChooser);*/
 
 
+        /*setupFactory();
+        setupChooser();*/
+    //}
+    
+
+    private static void setupFactory() {
+        /*AutoFactory.bind("Marker", Commands.print(
+                "Marker Passed"));*/
     }
+
+    private static void setupChooser() {}
+        // interface for choreo
+
+        // Made sendable, use SmartDashboard now
+//        chooser.addCmd("My Routine", () -> Autos.getMyRoutine());
+//        chooser.addCmd("Print", () -> Autos.getPrint());
+//        chooser.addCmd("Split", () -> Autos.getSplitRoutine());
+//        chooser.addCmd("Straight", () -> Autos.getStraight());
+//        // Default
+//        chooser.select("Straight");
+
 
     }
 
