435869c2fc6bc5f5fd9c1a11b2a39003d5c1507c
Tristan Rubio <triskit68@gmail.com>
2025-02-09 16:47:20 -0700

    NEW Trajs, 3L4C Top and Bottom

--- src/main/java/frc/robot/subsystems/AutoSubsystem/AutoSubsystem.java
+++ src/main/java/frc/robot/subsystems/AutoSubsystem/AutoSubsystem.java
@@ -204,15 +204,6 @@ public class AutoSubsystem extends SubsystemBase {
                 )
         );
 
-        //4L4TopCoral Chrono:
-//1 StarttoR11
-//2 R11toSource
-//3 SourcetoR12
-//4 R12toSource
-//5 SourcetoR1
-//6 R1toSource
-//7 SourcetoR2
-//8 R2toSource
 
         StarttoR11.atTime("StarttoR11").onTrue(drive());
         StarttoR11.done().onTrue(drive().andThen(R11toSource.cmd(), LowerElevator()));
@@ -243,6 +234,141 @@ public class AutoSubsystem extends SubsystemBase {
 
 
         return FourL4CoralTopRoutine;
+    }
+
+
+    public static AutoRoutine ThreeL4CoralBottom() {
+
+
+
+        AutoRoutine ThreeL4CoralBottomRoutine =
+                autoFactory.newRoutine(
+                        "ThreeL4CoralBottomRoutine");
+
+        AutoTrajectory StarttoR8 =
+                ThreeL4CoralBottomRoutine.trajectory("StarttoR8");
+//2
+        AutoTrajectory R8toSource =
+                ThreeL4CoralBottomRoutine.trajectory("R8toSource");
+//3
+        AutoTrajectory SourcetoR7 =
+                ThreeL4CoralBottomRoutine.trajectory("SourcetoR7");
+//4
+        AutoTrajectory R7toSource =
+                ThreeL4CoralBottomRoutine.trajectory("R7toSource");
+//5
+        AutoTrajectory SourcetoR6 =
+                ThreeL4CoralBottomRoutine.trajectory("SourcetoR6");
+
+
+
+
+        ThreeL4CoralBottomRoutine.active().onTrue(
+                Commands.sequence(
+                        Commands.print("Started" +
+                                "ThreeL4CoralBottomRoutine" +
+                                " the routine!"),
+                        StarttoR8.resetOdometry(),
+                        R8toSource.cmd(),
+                        SourcetoR7.cmd(),
+                        R7toSource.cmd(),
+                        SourcetoR6.cmd()
+                )
+        );
+
+        //4L4TopCoral Chrono:
+//1 StarttoR11
+//2 R11toSource
+//3 SourcetoR12
+//4 R12toSource
+//5 SourcetoR1
+//6 R1toSource
+//7 SourcetoR2
+//8 R2toSource
+
+        StarttoR8.atTime("StarttoR8").onTrue(drive());
+        StarttoR8.done().onTrue(drive().andThen(R8toSource.cmd(), LowerElevator()));
+
+        R8toSource.atTime("R8toSource").onTrue(deposit());
+        R8toSource.done().onTrue(drive().andThen(SourcetoR7.cmd(), RaiseElevator()));
+
+
+        SourcetoR7.atTime("SourcetoR7").onTrue(claw());
+        SourcetoR7.done().onTrue(drive().andThen(R7toSource.cmd(), LowerElevator()));
+
+        R7toSource.atTime("R7toSource").onTrue(deposit());
+        R7toSource.done().onTrue(drive().andThen(SourcetoR6.cmd(), RaiseElevator()));
+
+        SourcetoR6.atTime("SourcetoR6").onTrue(claw());
+        SourcetoR6.done();
+
+        System.out.println(StarttoR8.getInitialPose().get());
+
+
+        return ThreeL4CoralBottomRoutine;
+    }
+
+    public static AutoRoutine ThreeL4CoralTopRoutine() {
+
+        //        var trajectory = loadTrajectory(
+        //                "FourL4CoralBottom");
+
+        AutoRoutine ThreeL4CoralTopRoutine =
+                autoFactory.newRoutine(
+                        "ThreeL4CoralTopRoutine");
+
+        //Initialize
+        //1
+        AutoTrajectory StarttoR11 =
+                ThreeL4CoralTopRoutine.trajectory("StarttoR11");
+        //2
+        AutoTrajectory R11toSource =
+                ThreeL4CoralTopRoutine.trajectory("R11toSource");
+        //3
+        AutoTrajectory SourcetoR12 =
+                ThreeL4CoralTopRoutine.trajectory("SourcetoR12");
+        //4
+        AutoTrajectory R12toSource =
+                ThreeL4CoralTopRoutine.trajectory("R12toSource");
+        //5
+        AutoTrajectory SourcetoR1 =
+                ThreeL4CoralTopRoutine.trajectory("SourcetoR1");
+
+        ThreeL4CoralTopRoutine.active().onTrue(
+                Commands.sequence(
+                        Commands.print("Started" +
+                                "ThreeL4CoralTopRoutine" +
+                                " the routine!"),
+                        StarttoR11.resetOdometry(),
+                        R11toSource.cmd(),
+                        SourcetoR12.cmd(),
+                        R12toSource.cmd(),
+                        SourcetoR1.cmd()
+
+                )
+        );
+
+
+        StarttoR11.atTime("StarttoR11").onTrue(drive());
+        StarttoR11.done().onTrue(drive().andThen(R11toSource.cmd(), LowerElevator()));
+
+        R11toSource.atTime("R11toSource").onTrue(deposit());
+        R11toSource.done().onTrue(drive().andThen(SourcetoR12.cmd(), RaiseElevator()));
+
+
+        SourcetoR12.atTime("SourcetoR12").onTrue(claw());
+        SourcetoR12.done().onTrue(drive().andThen(R12toSource.cmd(), LowerElevator()));
+
+        R12toSource.atTime("R12toSource").onTrue(deposit());
+        R12toSource.done().onTrue(drive().andThen(SourcetoR1.cmd(), RaiseElevator()));
+
+        SourcetoR1.atTime("SourcetoR1").onTrue(claw());
+        SourcetoR1.done();
+
+        System.out.println(StarttoR11.getInitialPose().get());
+
+
+        return ThreeL4CoralTopRoutine;
     }
 }
 
