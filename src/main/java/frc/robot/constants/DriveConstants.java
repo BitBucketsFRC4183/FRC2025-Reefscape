a56738ca9778e9ce1c79d50ab2bda68188d7216f
Titus Nguyen <titus.tnguyen17@gmail.com>
2025-02-16 20:39:29 -0700

    can ids lol

--- src/main/java/frc/robot/constants/DriveConstants.java
+++ src/main/java/frc/robot/constants/DriveConstants.java
@@ -53,17 +53,17 @@ public class DriveConstants {
     public static final Rotation2d backRightZeroRotation = new Rotation2d(0.0);
 
     // Device CAN IDs
-    public static final int pigeonCanId = 9;
+    public static final int pigeonCanId = 11;
 
-    public static final int frontLeftDriveCanId = 1;
-    public static final int backLeftDriveCanId = 3;
-    public static final int frontRightDriveCanId = 5;
-    public static final int backRightDriveCanId = 7;
+    public static final int frontLeftDriveCanId = 6;
+    public static final int backLeftDriveCanId = 7;
+    public static final int frontRightDriveCanId = 8;
+    public static final int backRightDriveCanId = 9;
 
     public static final int frontLeftTurnCanId = 2;
-    public static final int backLeftTurnCanId = 4;
-    public static final int frontRightTurnCanId = 6;
-    public static final int backRightTurnCanId = 8;
+    public static final int backLeftTurnCanId = 3;
+    public static final int frontRightTurnCanId = 5;
+    public static final int backRightTurnCanId = 4;
 
     // Drive motor configuration
     public static final boolean driveInverted = false;
