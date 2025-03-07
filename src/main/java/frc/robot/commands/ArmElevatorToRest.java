e5625de66e036e22448f85632fae4e0f88f6d800
bbdriverstation2 <bbdriverstation@bitbuckets.org>
2025-03-07 14:38:03 -0800

    macros?

--- src/main/java/frc/robot/commands/ArmElevatorToRest.java
+++ src/main/java/frc/robot/commands/ArmElevatorToRest.java
@@ -1,27 +1,31 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import frc.robot.commands.ArmCommands.ArmBendCommand;
 import frc.robot.commands.ElevatorCommands.ElevatorSetPointCommand;
+import frc.robot.constants.ArmConstants;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.ArmSubsystem.ArmSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem.ElevatorSubsystem;
 
-public class ArmElevatorToSetpoint extends ParallelDeadlineGroup {
-    public ArmElevatorToSetpoint(ElevatorSubsystem elevator, ArmSubsystem arm, double elevatorSetpoint, double armSetpoint) {
+public class ArmElevatorToRest extends ParallelDeadlineGroup {
+    public ArmElevatorToRest(ElevatorSubsystem elevator, ArmSubsystem arm) {
         super(Commands.deadline(
                 Commands.waitSeconds(Constants.commandTimeout + 1),
                 Commands.sequence(
                         Commands.deadline(
-                                Commands.waitSeconds(0.1),
-                        new ElevatorSetPointCommand(elevator, elevatorSetpoint)),
+                                Commands.waitSeconds(0.2),
+                                new ArmBendCommand(arm, ArmConstants.MIN_ANGLE_RADS + Units.degreesToRadians(5)),
                         Commands.parallel(
-                                new ArmBendCommand(arm, armSetpoint),
-                                new ElevatorSetPointCommand(elevator, elevatorSetpoint))
-                                )
+                                new ArmBendCommand(arm, ArmConstants.MIN_ANGLE_RADS + Units.degreesToRadians(5)),
+                                new ElevatorSetPointCommand(elevator, -0.05))
+
                 )
+                )
+                )
+
         );
     }
 }
