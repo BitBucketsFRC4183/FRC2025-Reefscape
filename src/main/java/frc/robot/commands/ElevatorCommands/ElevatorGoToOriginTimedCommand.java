1435773e63da61ac8f6936cd7532343208be9353
sour <sour_was_here@kolabnow.com>
2025-03-14 16:05:17 -0700

    go to origin timed

---
+++ src/main/java/frc/robot/commands/ElevatorCommands/ElevatorGoToOriginTimedCommand.java
@@ -0,0 +1,35 @@
+package frc.robot.commands.ElevatorCommands;
+
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ElevatorSubsystem.ElevatorSubsystem;
+import org.littletonrobotics.junction.Logger;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ElevatorSubsystem.ElevatorSubsystem;
+import org.littletonrobotics.junction.Logger;
+
+public class ElevatorGoToOriginTimedCommand extends Command {
+    public ElevatorSubsystem elevator;
+    public double timeToCompleteSeconds;
+    // Constructor with parameters
+    public ElevatorGoToOriginTimedCommand(ElevatorSubsystem elevator, double timeToCompleteSeconds) {
+        this.elevator = elevator;
+        this.timeToCompleteSeconds = timeToCompleteSeconds;
+        addRequirements(elevator);
+    }
+
+    @Override
+    public void initialize() {
+        double velocity = elevator.getLoadHeight() / timeToCompleteSeconds;
+        this.elevator.elevatorPID.setGoal(new TrapezoidProfile.State(0, velocity));
+    }
+
+    @Override
+    public void execute(){
+        double calculatedVolts = elevator.elevatorFF.calculate(elevator.elevatorPID.getSetpoint().velocity) +
+                elevator.elevatorPID.calculate(elevator.getLoadHeight());
+        Logger.recordOutput("ElevatorSubsystem/target_voltage", calculatedVolts);
+        this.elevator.setElevatorVoltage(calculatedVolts);
+    }
+
+}
