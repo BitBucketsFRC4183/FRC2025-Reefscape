8723ec1e254ffb40d8a5fb7feebcc2c4ea59b017
Titus Nguyen <titus.tnguyen17@gmail.com>
2025-03-15 16:37:24 -0700

    janitorial merge work

--- src/main/java/frc/robot/subsystems/ArmSubsystem/ArmEncoderIOSim.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem/ArmEncoderIOSim.java
@@ -1,38 +1,16 @@
-package frc.robot.subsystems.SingleJointedArmSubsystem;
+package frc.robot.subsystems.ArmSubsystem;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.simulation.EncoderSim;
-import frc.robot.constants.SingleJointedArmConstants;
+import frc.robot.constants.ArmConstants;
+import frc.robot.subsystems.ArmSubsystem.ArmEncoderIO;
 
 import java.lang.Math;
 
-public class ArmEncoderIOSim extends EncoderSim {
-    private double lastTime = 0;
-    private double lastAngle = SingleJointedArmConstants.MIN_ANGLE;
-    private double velocityRadPerSec = 0;
-    private final double circumference = 2 * SingleJointedArmConstants.armLength * Math.PI; //2 * radius * pi
+public class ArmEncoderIOSim implements ArmEncoderIO {
+    public ArmEncoderIOSim() {
 
-    public ArmEncoderIOSim(Encoder encoder) {
-        super(encoder);
-    }
-
-    public double getVelocityRadPerSec() {
-        return (getAngle() - lastAngle) - (Timer.getFPGATimestamp() - lastTime);
-    }
-
-    public double getAngle() {
-        return (getDistance() / circumference) * (2 * Math.PI) + SingleJointedArmConstants.MIN_ANGLE;
-        //distance / circumference = percentage
-        //max is 2 pi radians
-        //percentage * max angle = current angle
-        //add minimum angle so measurements are as if minimum is 0
-    }
-
-    public void updateMeasurements() {
-        lastAngle = getAngle();
-        lastTime = Timer.getFPGATimestamp();
-        velocityRadPerSec = getVelocityRadPerSec();
     }
 
 }
