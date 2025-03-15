cf3c515d7197ccbaf66a6caf8b148c84ac37d21d
sour <sour_was_here@kolabnow.com>
2025-03-15 16:13:11 -0700

    change sim encoder measurements to angles

--- src/main/java/frc/robot/subsystems/ArmSubsystem/ArmEncoderIOSim.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem/ArmEncoderIOSim.java
@@ -3,22 +3,36 @@ package frc.robot.subsystems.SingleJointedArmSubsystem;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.simulation.EncoderSim;
+import frc.robot.constants.SingleJointedArmConstants;
+
+import java.lang.Math;
 
 public class ArmEncoderIOSim extends EncoderSim {
     private double lastTime = 0;
-    private double lastDistance = 0;
+    private double lastAngle = SingleJointedArmConstants.MIN_ANGLE;
+    private double velocityRadPerSec = 0;
+    private final double circumference = 2 * SingleJointedArmConstants.armLength * Math.PI; //2 * radius * pi
 
     public ArmEncoderIOSim(Encoder encoder) {
         super(encoder);
     }
 
-    public double getVelocityInSeconds() {
-        return (getDistance() - lastDistance) - (Timer.getFPGATimestamp() - lastTime);
+    public double getVelocityRadPerSec() {
+        return (getAngle() - lastAngle) - (Timer.getFPGATimestamp() - lastTime);
+    }
+
+    public double getAngle() {
+        return (getDistance() / circumference) * (2 * Math.PI) + SingleJointedArmConstants.MIN_ANGLE;
+        //distance / circumference = percentage
+        //max is 2 pi radians
+        //percentage * max angle = current angle
+        //add minimum angle so measurements are as if minimum is 0
     }
 
     public void updateMeasurements() {
-        lastDistance = getDistance();
+        lastAngle = getAngle();
         lastTime = Timer.getFPGATimestamp();
+        velocityRadPerSec = getVelocityRadPerSec();
     }
 
 }
