bf8ee2a4c2c815841c7205438853843c6a9d7265
Tylefrost <114878022+Tylefrost@users.noreply.github.com>
2025-03-15 17:10:14 -0700

    ArmEncoderSim fixed

--- src/main/java/frc/robot/subsystems/ArmSubsystem/ArmEncoderIOSim.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem/ArmEncoderIOSim.java
@@ -1,16 +1,27 @@
 package frc.robot.subsystems.ArmSubsystem;
 
+import edu.wpi.first.math.filter.LinearFilter;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 import edu.wpi.first.wpilibj.simulation.EncoderSim;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import frc.robot.constants.ArmConstants;
 import frc.robot.subsystems.ArmSubsystem.ArmEncoderIO;
 
-import java.lang.Math;
 
 public class ArmEncoderIOSim implements ArmEncoderIO {
-    public ArmEncoderIOSim() {
+    private final SingleJointedArmSim sim;
+    public ArmEncoderIOSim(SingleJointedArmSim sim) {
+        this.sim = sim;
+    }
 
+    @Override
+    public void updateInputs(ArmEncoderIO.ArmEncoderIOInputs inputs) {
+        inputs.armAngle = sim.getAngleRads();
+        inputs.armAngleDegs = Units.radiansToDegrees(inputs.armAngle);
+        inputs.armVelocityRads = sim.getVelocityRadPerSec();
     }
 
 }
