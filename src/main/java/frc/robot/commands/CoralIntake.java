a05b8b7851e8cc6836be3c7aa69c0fe763de7163
mslonglife <ms.long.life@gmail.com>
2025-03-23 23:26:56 -0700

    bros' error

--- src/main/java/frc/robot/commands/CoralIntake.java
+++ src/main/java/frc/robot/commands/CoralIntake.java
@@ -1,8 +1,11 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.commands.ClawCommands.ClawIntakeCommand;
+import frc.robot.subsystems.ArmSubsystem.ArmSubsystem;
 import frc.robot.subsystems.ClawSubsystem.ClawSubsystem;
 import frc.robot.subsystems.ElevatorSubsystem.ElevatorSubsystem;
+
 
 
 public class CoralIntake {
