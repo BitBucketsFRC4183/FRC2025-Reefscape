445ba8b658689ac474e1756bbf626f3237087503
bbdriverstation2 <bbdriverstation@bitbuckets.org>
2025-03-27 10:22:24 -0700

    remove center wheel

--- src/main/java/frc/robot/subsystems/ClawSubsystem/EndEffectorIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/ClawSubsystem/EndEffectorIOSparkMax.java
@@ -15,13 +15,13 @@ import static frc.robot.util.SparkUtil.tryUntilOk;
 
 public class EndEffectorIOSparkMax implements EndEffectorIO {
     private final SparkMax gripperWheels; //hold object
-    private final SparkMax centralWheel; //open and close claw
+    // private final SparkMax centralWheel; //open and close claw
     private boolean hasAlgae = false;
     private boolean hasCoral = false;
     private boolean isOpen = false;
 //this should be fine? we'll see
     public EndEffectorIOSparkMax() {
-        centralWheel = new SparkMax(ClawConstants.centralID, SparkLowLevel.MotorType.kBrushless); //big
+        // centralWheel = new SparkMax(ClawConstants.centralID, SparkLowLevel.MotorType.kBrushless); //big
         gripperWheels = new SparkMax(ClawConstants.wheelsID, SparkLowLevel.MotorType.kBrushless); //small
         SparkMaxConfig clawConfig = new SparkMaxConfig();
         clawConfig
@@ -35,12 +35,12 @@ public class EndEffectorIOSparkMax implements EndEffectorIO {
                 () ->
                         gripperWheels.configure(
                                 clawConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters));
-        tryUntilOk(centralWheel
-                ,
-                5,
-                () ->
-                        gripperWheels.configure(
-                                clawConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters));
+//        tryUntilOk(centralWheel
+//                ,
+//                5,
+//                () ->
+//                        gripperWheels.configure(
+//                                clawConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters));
     }
 
     public boolean getHasCoral() { return this.hasCoral; }
@@ -65,12 +65,12 @@ public class EndEffectorIOSparkMax implements EndEffectorIO {
 
     @Override
     public void setCentralVelocity(double velocity) {
-        centralWheel.set(velocity);
+        //centralWheel.set(velocity);
     }
 
     @Override
     public void setCentralVoltage(double volts) {
-        centralWheel.setVoltage(volts);
+        // centralWheel.setVoltage(volts);
         if (volts == ClawConstants.mainVoltageTarget) {
             setHasCoral(false);
         } else if (volts == 0) { //if closing
@@ -85,7 +85,7 @@ public class EndEffectorIOSparkMax implements EndEffectorIO {
 
     @Override
     public void updateInputs(EndEffectorInputsAutoLogged inputs) {
-        inputs.centralVolts = centralWheel.getAppliedOutput();
+        // inputs.centralVolts = centralWheel.getAppliedOutput();
         inputs.gripperVolts = gripperWheels.getAppliedOutput();
         inputs.hasCoral = getHasCoral();
         inputs.hasAlgae = getHasAlgae();
